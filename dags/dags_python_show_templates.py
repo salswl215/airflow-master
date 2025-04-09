3a018bb124ad1a7a24c8ced89fcbfc7b7b8026ca
salswl215 <lyn7666@gmail.com>
2025-04-09 22:05:47 +0900

    upload timestamp print dag

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+from pprint import pprint
+
+with DAG(
+        dag_id="dags_python_show_templates",
+        schedule="30 9 * * *",
+        start_date=pendulum.datetime(2025, 4, 1, tz="Asia/Seoul"),
+        catchup=True
+) as dag:
+
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        pprint(kwargs)
+
+    show_templates()
