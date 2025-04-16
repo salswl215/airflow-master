934e4184af37b40da08641a6c4a4de74382eeb8c
salswl215 <lyn7666@gmail.com>
2025-04-16 21:59:19 +0900

    upload http operator

---
+++ dags/dags_http_operator.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import HttpOperator
+from airflow.decorators import task
+import pendulum
+
+with DAG(
+    dag_id='dags_http_operator',
+    start_date=pendulum.datetime(2025, 4, 16, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None
+) as dag:
+    '''서울시 공공자전거 대여소 정보'''
+    tb_cycle_station_use_month_info = HttpOperator(
+        task_id='tb_cycle_station_use_month_info',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationUseMonthInfo/1/5/202501',
+        method='GET',
+        headers={'Content-Type': 'application/json',
+                 'charset': 'utf-8',
+                 'Accept': '*/*'
+                 }
+    )
+
+    @task(task_id = 'python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='tb_cycle_station_use_month_info')
+        import json
+        from pprint import pprint
+
+        pprint(json.loads(rslt))
+
+    tb_cycle_station_use_month_info >> python_2()
