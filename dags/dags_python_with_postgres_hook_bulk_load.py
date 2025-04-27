663624802e148fedf84aeb2a690af69bb4e7790b
salswl215 <lyn7666@gmail.com>
2025-04-27 21:27:11 +0900

    uploads dags with postgres hook - bulk load

---
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+with DAG(
+    dag_id = 'dags_python_with_postgres_hook_bulk_load',
+    start_date = pendulum.datetime(2025, 4, 27, tz = 'Asia/Seoul'),
+    schedule = '0 7 * * *',
+    catchup = False
+) as dag:
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+    insrt_postgres = PythonOperator(
+        task_id = 'insrt_postgres',
+        python_callable = insrt_postgres,
+        op_kwargs={
+            'postgres_conn_id' : 'conn-db-postgres-custom',
+            'tbl_nm': 'SearchParkInfoService_bulk1',
+            'file_nm':'/opt/airflow/files/SearchParkInfoService/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/SearchParkInfoService.csv'}'
+        }
+
+    )
