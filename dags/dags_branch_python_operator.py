d789e0a513f3cae3222df23a52066324d4289f67
salswl215 <lyn7666@gmail.com>
2025-05-01 21:43:23 +0900

    upload external task sensor

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -6,7 +6,7 @@ from airflow.operators.python import BranchPythonOperator
 
 with DAG(
     dag_id="dags_branch_python_operator",
-    schedule=None,
+    schedule='0 1 * * *',
     start_date=pendulum.datetime(2025, 4, 13, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
