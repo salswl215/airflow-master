d3c9b0ef17e6ae654cdb6f548217d4d84da0a387
salswl215 <lyn7666@gmail.com>
2025-05-01 21:52:03 +0900

    fix typo of external sensor dag

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -6,7 +6,7 @@ from airflow.utils.state import State
 
 with DAG(
     dag_id='dags_external_task_sensor',
-    start_date=pendulum.datetime(2025,5,1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2025,4,30, tz='Asia/Seoul'),
     schedule='0 7 * * *',
     catchup=False
 ) as dag:
