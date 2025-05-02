b6afd7532aa0e935c15d7dada07e549c4d49b62a
salswl215 <lyn7666@gmail.com>
2025-05-02 21:00:19 +0900

    fixbug dag (checking triggerer actions)

--- dags/dags_time_sensor_with_async.py
+++ dags/dags_time_sensor_with_async.py
@@ -1,6 +1,6 @@
 import pendulum
 from airflow import DAG
-from airflow.sensors.date_time import DateTimeSensor
+from airflow.sensors.date_time import DateTimeSensorAsync
 
 with DAG(
     dag_id = 'dags_time_sensor_with_async',
@@ -9,7 +9,7 @@ with DAG(
     schedule="*/10 * * * *",
     catchup=True,
 )as dag:
-    sync_sensor = DateTimeSensor(
+    sync_sensor = DateTimeSensorAsync(
         task_id = 'sync_sensor',
         target_time="""{{ macros.datetime.utcnow() + macros.timedelta(minutes=2) }}""",
     )
