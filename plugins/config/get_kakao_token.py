84be16a32a555ca3ea5799de764e36b7cd34e09d
salswl215 <lyn7666@gmail.com>
2025-05-06 17:18:50 +0900

    hide secret info

--- plugins/config/get_kakao_token.py
+++ plugins/config/get_kakao_token.py
@@ -1,9 +1,9 @@
 import requests
 # client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
 
-client_id = '2396b69529de66e1cab1759c9774fb21'
+client_id = '{client_id}'
 redirect_uri = 'https://example.com/oauth'
-authorize_code = 'W1XO-xsf4jYxCYtapfgwL1xMQ4DygB2tynkqKNs6Wc4apHCCo-CxGwAAAAQKFxafAAABlqSjgwvkNSpXBP-m7Q'
+authorize_code = '{authorize_code}'
 
 
 token_url = 'https://kauth.kakao.com/oauth/token'
